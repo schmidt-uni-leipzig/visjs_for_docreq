05e7a7ac411e27b8413225b260c3f7c4d96490f7
Stefan Bechert <bechert@infai.org>
2015-10-19 17:17:44 +0200

    tarjan works

--- visjs-utils.service.js
+++ visjs-utils.service.js
@@ -13,6 +13,7 @@ angular.module('testApp')
     .service('visUtils', function () {
 
 
+        var selectionForPathBool = false;
         var settings;
         var network;
         var nodes;
@@ -20,6 +21,8 @@ angular.module('testApp')
         var nodeIndex = [];
         var selectionForPath = [];
         var stabilizeTimer = 0;
+
+        var stronglyConnectedComponents = [];
 
 
         var serviceObject = this;
@@ -109,6 +112,7 @@ angular.module('testApp')
             network = new vis.Network(container, data, settings.options);
 
             network.on('stabilized', onStabilized);
+            network.on('click', onClick);
             network.moveTo({
                 scale: 0.00000000000000000000000000000000000000001
             });
@@ -122,174 +126,173 @@ angular.module('testApp')
             }
         };
 
-        serviceObject.updateData = function(edgePack, nodePack) {
+        serviceObject.updateData = function (edgePack, nodePack) {
 
             nodes.update(edgePack);
             edges.update(nodePack);
         };
 
-        serviceObject.setPathOrSelectionMode = function(choice) {
+        serviceObject.setPathOrSelectionMode = function (choice) {
+
+            if (choice === 'path') {
+                selectionForPathBool = true;
+                selectionForPath = [];
+
+            } else {
+                selectionForPathBool = false;
+                selectionForPath = [];
+            }
 
             var allNodes = nodes.get();
             var allEdges = edges.get();
             restoreOnUnselect(allNodes, allEdges, settings);
 
-            if(choice === 'path') {
-                network.off('click', onClick);
-                network.on('select', onSelect);
-                selectionForPath = [];
-
-            } else {
-                network.on('click', onClick);
-                network.off('select', onSelect);
-                selectionForPath = [];
-            }
-
             nodes.update(allNodes);
             edges.update(allEdges);
         };
 
+        var count = 0;
+        var stack = [];
 
-        /**
-         * Highlights the selected node and direct neighbours.
-         * @param selectedItems
-         */
+
         function onClick(selectedItems) {
 
             var allNodes = nodes.get();
             var allEdges = edges.get();
 
+            if (!selectionForPathBool) {
+                if (selectedItems.nodes.length !== 1) {
+                    restoreOnUnselect(allNodes, allEdges, settings);
+                } else {
+                    for (var i = 0; i < allEdges.length; i++) {
+                        allEdges[i].inConnectionList = (allEdges[i].from === selectedItems.nodes[0] || allEdges[i].to === selectedItems.nodes[0]);
+                    }
 
+                    clearLevelOfSeperation(allNodes); //reset nodes
 
-            if (selectedItems.nodes.length !== 1) {
-                restoreOnUnselect(allNodes, allEdges, settings);
-            } else {
-                for (var i = 0; i < allEdges.length; i++) {
-                    allEdges[i].inConnectionList = (allEdges[i].from === selectedItems.nodes[0] || allEdges[i].to === selectedItems.nodes[0]);
+                    var ids = getConnectedNodes(selectedItems.nodes[0], allEdges, true);
+                    ids.push(selectedItems.nodes[0]);
+
+                    storeLevelOfSeperation(ids, allNodes); //save node states
+
+                    setColor(allNodes, allEdges, true);
                 }
 
-                clearLevelOfSeperation(allNodes); //reset nodes
 
-                var ids = getConnectedNodes(selectedItems.nodes[0], allEdges, true);
-                ids.push(selectedItems.nodes[0]);
-
-                storeLevelOfSeperation(ids, allNodes); //save node states
-
-                setColor(allNodes, allEdges, true);
-            }
-
-            nodes.update(allNodes);
-            edges.update(allEdges);
-        }
-
-        /**
-         * Selected Nodes are saved in var selectionForPath. After selecting two Nodes
-         * it will start a depth-first search, which calculates and highlights all possible ways.
-         *
-         * @param selectedItems
-         */
-        function onSelect(selectedItems) {
-
-            var allNodes = nodes.get();
-            var allEdges = edges.get();
-
-            var hide = false;
-
-            //reset Edges
-            for (var i = 0; i < allEdges.length; i++) {
-                allEdges[i].inConnectionList = false;
-            }
-
-            //if 'blank' click or third selection -> reset
-            if ((!selectedItems.nodes[selectedItems.nodes.length - 1]) || selectionForPath.length > 2) {
-                selectionForPath = [];
-                restoreOnUnselect(allNodes, allEdges, settings);
             } else {
-                for (var p = 0; p < allNodes.length; p++) {
-                    allNodes[p].checked = false;
-                    if (allNodes[p].id === selectedItems.nodes[selectedItems.nodes.length - 1]) {
-                        allNodes[p].inConnectionList = true;
-                        selectionForPath.push(allNodes[p]);
+                var hide = false;
+
+                //reset Edges
+                for (var i = 0; i < allEdges.length; i++) {
+                    allEdges[i].inConnectionList = false;
+                }
+
+                //if 'blank' click or third selection -> reset
+                if ((!selectedItems.nodes[selectedItems.nodes.length - 1]) || selectionForPath.length > 2) {
+                    selectionForPath = [];
+                    restoreOnUnselect(allNodes, allEdges, settings);
+                } else {
+                    for (var p = 0; p < allNodes.length; p++) {
+                        allNodes[p].checked = false;
+                        if (allNodes[p].id === selectedItems.nodes[selectedItems.nodes.length - 1]) {
+                            allNodes[p].inConnectionList = true;
+                            selectionForPath.push(allNodes[p]);
+                        }
                     }
                 }
+
+                //if two selected -> calculate path
+                if (selectionForPath.length === 2) {
+
+                    //TODO
+                    console.log('calculating..');
+
+                    var temp = angular.copy(allNodes);
+                    setAllNodesToUnvisited(temp);
+                    stronglyConnectedComponents = [];
+
+                    for (i = 0; i < temp.length; i++) {
+                        count = 0;
+                        stack = [];
+
+                        if (temp[i].state === 'unchecked') {
+                            tarjanForStronglyConnectedComponents(temp, allEdges, temp[i]);
+                        }
+                    }
+
+                    console.log(stronglyConnectedComponents);
+
+
+                    selectionForPath = [];
+                }
+
+                setColor(allNodes, allEdges, hide);
             }
 
-            //if two selected -> calculate path
-            if (selectionForPath.length === 2) {
 
-                //TODO
-                //var temp = angular.copy(allNodes);
-                //setAllNodesToUnvisited(temp);
-                //for(i = 0; i < temp.length; i++){
-                //    if(temp[i].state === 'unchecked'){
-                //        tarjanForStronglyConnectedComponents(temp, allEdges, temp[i]);
-                //    }
-                //}
-
-                selectionForPath = [];
-            }
-
-            setColor(allNodes, allEdges, hide);
             nodes.update(allNodes);
             edges.update(allEdges);
         }
 
+        function setAllNodesToUnvisited(allNodes) {
 
-        //var count = 0;
-        //var stack = [];
-
-        //function setAllNodesToUnvisited(allNodes) {
-        //
-        //    count = 0;
-        //    stack = [];
-        //
-        //    for(var i = 0; i < allNodes.length; i++){
-        //        allNodes[i].state = 'unchecked';
-        //    }
-        //}
+            for (var i = 0; i < allNodes.length; i++) {
+                allNodes[i].state = 'unchecked';
+            }
+        }
 
 
-        //function tarjanForStronglyConnectedComponents(allNodes, allEdges, currentNode) {
-
-            //currentNode = allNodes[0]
+        function tarjanForStronglyConnectedComponents(allNodes, allEdges, currentNode) {
 
             //possible states: unchecked, checked, finished
-            //currentNode.state = 'checked';
-            //console.log(allNodes);
-            //
-            //count++;
-            //currentNode.in = count;
-            //currentNode.l = count;
-            //
-            //stack.push(currentNode);
-            //currentNode.onStack = true;
-            //
-            //var su = succ(currentNode.id, allEdges);
-            //
-            //for(var i = 0; i < su.length; i++) {
-            //    var next = getNodeById(su[i], allNodes);
-            //    //console.log(next);
-            //
-            //    if(next.state === 'unchecked') {
-            //        tarjanForStronglyConnectedComponents(allNodes, allEdges, next);
-            //        currentNode.l = currentNode.l > next.l ? next.l : currentNode.l;
-            //    } else if(next.onStack) {
-            //        currentNode.l = currentNode.l > next.in ? next.in : currentNode.l;
-            //    }
-            //
-            //}
-            //
-            //if(currentNode.l === currentNode.in) {
-            //    console.log('starke Szkomponente:');
-            //    while(stack[stack.length - 1].id !== currentNode.id) {
-            //        var w = stack.pop();
-            //        w.onStack = false;
-            //        console.log(w);
-            //    }
-            //}
-            //
-            //currentNode.state = 'finished';
-            //count++;
+            //console.log('current:', currentNode.label);
+            currentNode.state = 'checked';
+
+            count++;
+            currentNode.in = count;
+            currentNode.l = count;
+
+            stack.push(currentNode);
+            currentNode.onStack = true;
+
+            var su = succ(currentNode.id, allEdges);
+
+            for (var i = 0; i < su.length; i++) {
+                var next = getNodeById(su[i], allNodes);
+                //console.log(next);
+
+                if (!next.onStack) {
+                    tarjanForStronglyConnectedComponents(allNodes, allEdges, next);
+                    currentNode.l = currentNode.l > next.l ? next.l : currentNode.l;
+                } else {
+                    currentNode.l = currentNode.l > next.in ? next.in : currentNode.l;
+                }
+
+            }
+
+            if (currentNode.l === currentNode.in) {
+
+                var component = [];
+
+                //console.log('starke Szkomponente:');
+                while (stack[stack.length - 1].id !== currentNode.id) {
+                    var w = stack.pop();
+                    w.onStack = false;
+                    component.push(w);
+                    //console.log(w);
+                }
+
+                var w = stack.pop();
+                w.onStack = false;
+                component.push(w);
+
+
+                addComponentWithoutDuplicate(component);
+                //console.log(w);
+            }
+
+            currentNode.state = 'finished';
+            count++;
 
             //Tarjan-visit(G,v){
             //    farbe[v]=grau; zeit=zeit+1; in[v]=zeit; l[v]=zeit;
@@ -306,16 +309,42 @@ angular.module('testApp')
             //    }
             //    farbe[v]=schwarz; zeit=zeit+1;
             //}
-        //}
+        }
 
-        //function getNodeById(nodeId, allNodes) {
-        //     for( var i = 0; i < allNodes.length; i++) {
-        //         if(allNodes[i].id === nodeId){
-        //             return allNodes[i];
-        //         }
-        //     }
-        //    return null;
-        //}
+        function addComponentWithoutDuplicate(comp) {
+
+            var valid = true;
+
+            angular.forEach(stronglyConnectedComponents , function (current) {
+
+
+                    //if length matches -> check in detail
+                    if (current.length === comp.length) {
+
+                        for (var j = 0; j < comp.length; j++) {
+
+                            if (current[0].id === comp[j].id) {
+                                valid = false;
+                            }
+                        }
+                    }
+                }
+            );
+
+            if(valid) {
+                stronglyConnectedComponents.push(comp);
+            }
+
+        }
+
+        function getNodeById(nodeId, allNodes) {
+            for (var i = 0; i < allNodes.length; i++) {
+                if (allNodes[i].id === nodeId) {
+                    return allNodes[i];
+                }
+            }
+            return null;
+        }
 
         /**
          * Sets all Nodes (Color and Label) on unselect (!visjs can still
@@ -328,7 +357,7 @@ angular.module('testApp')
             for (nodeId in allNodes) {
                 if (allNodes.hasOwnProperty(nodeId)) {
                     if (settings.useImage) {
-                        allNodes[nodeId].image = settings.imagePath  + allNodes[nodeId].type + '-icon.png';
+                        allNodes[nodeId].image = settings.imagePath + allNodes[nodeId].type + '-icon.png';
                     } else {
                         allNodes[nodeId].color = settings.colDefault;
                     }
@@ -352,9 +381,9 @@ angular.module('testApp')
         function storeLevelOfSeperation(nodeIdsForHighlighting, allNodes) {
 
             for (var i = 0; i < nodeIdsForHighlighting.length; i++) {
-                for(var j = 0; j < allNodes.length; j++){
+                for (var j = 0; j < allNodes.length; j++) {
 
-                    if(allNodes[j].id === nodeIdsForHighlighting[i]){
+                    if (allNodes[j].id === nodeIdsForHighlighting[i]) {
                         allNodes[j].levelOfSeperation = 1;
                         allNodes[j].inConnectionList = true;
                     }
@@ -379,55 +408,6 @@ angular.module('testApp')
         }
 
         /**
-         * Starts a depth-first search and returns an array containing
-         * all possible ways between two nodes.
-         * @param {int} nodeId
-         * @param {[object]} allNodes
-         * @param {[object]} allEdges
-         * @param {int} targetId
-         * @returns {*}
-         */
-        //function depthFirstSearch(nodeId, allNodes, allEdges, targetId, nodeIndex) {
-        //
-        //    var nodePos = givePos(nodeId, nodeIndex);
-        //    //if found
-        //    if (nodeId === targetId) {
-        //        console.log('found node: ' + allNodes[givePos(nodeId, nodeIndex)].label);
-        //        return [[nodeId]];
-        //    }
-        //    var neighbours = getConnectedNodes(nodeId, allEdges, false);
-        //
-        //    //if leaf
-        //    if (neighbours.length === 0) {
-        //        console.log('it is a leave -> stop!');
-        //        return [];
-        //        //if cycle
-        //    } else if (allNodes[nodePos].checked) {
-        //        console.log('we are moving in a cycle ... ');
-        //
-        //        return [];
-        //
-        //    } else {
-        //        console.log('neighbours are ' + neighbours);
-        //        var paths = [];
-        //        allNodes[nodePos].checked = true;
-        //
-        //        for (var i = 0; i < neighbours.length; i++) {
-        //            console.log('checking node ' + allNodes[givePos(neighbours[i], nodeIndex)].label);
-        //            var temp = depthFirstSearch(neighbours[i], allNodes, allEdges, targetId, nodeIndex);
-        //
-        //            for (var k = 0; k < temp.length; k++) {
-        //                temp[k].push(nodeId);
-        //                paths.push(temp[k]);
-        //            }
-        //
-        //        }
-        //        allNodes[nodePos].checked = false;
-        //        return paths;
-        //    }
-        //}
-
-        /**
          * Get a list of nodes that are connected to the supplied nodeId with edges.
          * @param nodeId, bothDirections
          * @returns {Array of Ids}
@@ -447,19 +427,19 @@ angular.module('testApp')
             return connectedNodes;
         }
 
-        //function succ(nodeId, allEdges) {
-        //
-        //    var connectedNodes = [];
-        //
-        //    for (var i = 0; i < allEdges.length; i++) {
-        //        var edge = allEdges[i];
-        //
-        //        if(edge.from === nodeId) {
-        //            connectedNodes.push(edge.to);
-        //        }
-        //    }
-        //    return connectedNodes;
-        //}
+        function succ(nodeId, allEdges) {
+
+            var connectedNodes = [];
+
+            for (var i = 0; i < allEdges.length; i++) {
+                var edge = allEdges[i];
+
+                if (edge.from === nodeId) {
+                    connectedNodes.push(edge.to);
+                }
+            }
+            return connectedNodes;
+        }
 
         /**
          * Sets color for Nodes and Edges
@@ -523,4 +503,6 @@ angular.module('testApp')
         }
 
         return serviceObject;
-    });
+    }
+)
+;
